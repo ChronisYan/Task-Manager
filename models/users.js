6512e59f6af67fb4631c7b8811193ab72d97940f
ChronisG <thegamedevelopergame@gmail.com>
2020-07-06 02:47:32 +0100

    update users model

---
+++ models/users.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 15,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  },
+});
